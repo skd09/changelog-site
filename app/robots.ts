4231d9a3cdf3f37e85d90585e0556b498bae164c
Sharvari <sharvaridivekar09@gmail.com>
2026-03-11 20:47:53 -0700

    feat: security files

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://sharvari.dev/sitemap.xml',
+  }
+}
